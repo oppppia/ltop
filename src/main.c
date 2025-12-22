e12d79de868746b48f2b6c1846b509422a24b0f2
oppppia <dmitriymarlowpierrebourne@gmail.com>
2025-12-22 03:59:42 +0300

    fix: Remove useless colors

--- src/main.c
+++ src/main.c
@@ -398,13 +398,6 @@ static void init_ncurses(void) {
     keypad(stdscr, TRUE);
     curs_set(0);
 
-    if (has_colors()) {
-        start_color();
-        init_pair(1, COLOR_RED, COLOR_BLACK);
-        init_pair(2, COLOR_GREEN, COLOR_BLACK);
-        init_pair(3, COLOR_YELLOW, COLOR_BLACK);
-    }
-
     timeout(REFRESH_INTERVAL_MS);
 }
 
